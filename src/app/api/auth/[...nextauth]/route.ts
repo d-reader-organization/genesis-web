53c1243d0a161eeb9362e0b944c324b53250c536
d-reader-luka <luka.crnogorac@dreader.io>
2024-07-11 16:39:51 +0200

    try

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,43 @@
-import { authOptions } from '@/utils/auth'
+import { accessTokenKey, googleAccessTokenKey } from '@/constants/general'
+import { RoutePath } from '@/enums/routePath'
+import { Authorization } from '@/models/auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { cookies } from 'next/headers'
 import NextAuth from 'next-auth'
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_AUTH_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+  session: { strategy: 'jwt' },
+  callbacks: {
+    signIn: async ({ account }) => {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/login-with-google`, {
+        method: 'PATCH',
+        headers: {
+          authorization: `Google ${account?.access_token}`,
+        },
+      })
+      const parsed: Authorization | string = await response.json()
+      if (typeof parsed === 'string') {
+        cookies().set(googleAccessTokenKey, account?.access_token ?? '')
+        return parsed
+      }
+
+      cookies().set(accessTokenKey, parsed.accessToken)
+      return RoutePath.Home
+    },
+  },
+})
 
 export { handler as GET, handler as POST }
