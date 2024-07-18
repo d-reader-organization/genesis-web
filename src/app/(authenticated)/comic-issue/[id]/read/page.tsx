dd342a661700ec86e53b614943fd404eb05e7f33
d-reader-luka <luka.crnogorac@dreader.io>
2024-07-18 08:46:18 +0200

    build fix

--- src/app/(authenticated)/comic-issue/[id]/read/page.tsx
+++ src/app/(authenticated)/comic-issue/[id]/read/page.tsx
@@ -1,5 +1,5 @@
+import { ComicIssuePageParams } from '@/models/common'
 import React from 'react'
-import { ComicIssuePageParams } from '../page'
 
 export default async function ReadComicIssuePage({}: ComicIssuePageParams) {
   return <div>Read page</div>
