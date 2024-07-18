20e41abd15f96a97b7f963c1ce5f4ae5eb6247b9
d-reader-luka <luka.crnogorac@dreader.io>
2024-07-18 13:01:19 +0200

    signup banner

--- src/app/(unauthenticated)/mint/[id]/page.tsx
+++ src/app/(unauthenticated)/mint/[id]/page.tsx
@@ -1,6 +1,11 @@
+import { SignUpBanner } from '@/components/shared/SignUpBanner'
 import { ComicIssuePageParams } from '@/models/common'
 import React from 'react'
 
-export default async function MintPage(params: ComicIssuePageParams) {
-  return <div>{params.params.id}</div>
+export default async function MintPage({ params }: ComicIssuePageParams) {
+  return (
+    <div className='mt-10 px-4 md:px-8 max-w-screen-xl mb-8'>
+      <SignUpBanner comicIssueId={params.id} />
+    </div>
+  )
 }
