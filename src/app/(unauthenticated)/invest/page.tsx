cb225e61e49cb2bbeead63b7a77c76d171cc3b7f
d-reader-luka <luka.crnogorac@dreader.io>
2024-09-17 09:05:28 +0200

    dummy invest page

---
+++ src/app/(unauthenticated)/invest/page.tsx
@@ -0,0 +1,9 @@
+import { BaseLayout } from '@/components/layout/BaseLayout'
+
+export default async function InvestPage() {
+  return (
+    <BaseLayout>
+      <div className='flex flex-col items-center'>Invest page</div>
+    </BaseLayout>
+  )
+}
