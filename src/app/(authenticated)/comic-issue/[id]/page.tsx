473f8b51486700b40e3459573a72fae2d5202387
d-reader-luka <luka.crnogorac@dreader.io>
2024-09-30 15:51:37 +0200

    fix comic issue page

--- src/app/(authenticated)/comic-issue/[id]/page.tsx
+++ src/app/(authenticated)/comic-issue/[id]/page.tsx
@@ -6,6 +6,7 @@ import { ComicIssueInfoSection } from '@/components/comicIssue/InfoSection'
 import { Navigation } from '@/components/layout/Navigation'
 import { ComicIssuePageParams } from '@/models/common'
 import React from 'react'
+import { CandyMachineStoreProvider } from '@/providers/CandyMachineStoreProvider'
 
 export default async function ComicIssuePage({ params: { id } }: ComicIssuePageParams) {
   const comicIssue = await fetchComicIssue(id)
@@ -18,7 +19,9 @@ export default async function ComicIssuePage({ params: { id } }: ComicIssuePageP
         <ComicIssueBanner cover={comicIssue.cover} />
         <div className='px-4 md:px-8 max-w-screen-xl mb-8'>
           <ComicIssueHeader>
-            <ComicIssueInfoSection comicIssue={comicIssue} />
+            <CandyMachineStoreProvider comicIssue={comicIssue}>
+              <ComicIssueInfoSection comicIssue={comicIssue} />
+            </CandyMachineStoreProvider>
             <ComicIssueBottomSection comicIssue={comicIssue} />
           </ComicIssueHeader>
           {comicIssue.isSecondarySaleActive && <p>secondary market</p>}
