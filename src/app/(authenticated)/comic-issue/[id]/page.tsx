29a8d59b394be29ba7a45241166e4c957c95840d
d-reader-luka <luka.crnogorac@dreader.io>
2024-10-02 11:07:09 +0200

    fix comic issue page layout

--- src/app/(authenticated)/comic-issue/[id]/page.tsx
+++ src/app/(authenticated)/comic-issue/[id]/page.tsx
@@ -3,30 +3,25 @@ import { ComicIssueBottomSection } from '@/components/comicIssue/BottomSection'
 import { ComicIssueBanner } from '@/components/comicIssue/Banner'
 import { ComicIssueHeader } from '@/components/comicIssue/Header'
 import { ComicIssueInfoSection } from '@/components/comicIssue/InfoSection'
-import { Navigation } from '@/components/layout/Navigation'
 import { ComicIssuePageParams } from '@/models/common'
 import React from 'react'
 import { CandyMachineStoreProvider } from '@/providers/CandyMachineStoreProvider'
+import { BaseLayout } from '@/components/layout/BaseLayout'
 
 export default async function ComicIssuePage({ params: { id } }: ComicIssuePageParams) {
   const comicIssue = await fetchComicIssue(id)
   if (!comicIssue || !comicIssue.stats || !comicIssue.myStats) return null
 
   return (
-    <>
-      <Navigation />
-      <main className=''>
-        <ComicIssueBanner cover={comicIssue.cover} />
-        <div className='px-4 md:px-8 max-w-screen-xl mb-8'>
-          <ComicIssueHeader>
-            <CandyMachineStoreProvider comicIssue={comicIssue}>
-              <ComicIssueInfoSection comicIssue={comicIssue} />
-            </CandyMachineStoreProvider>
-            <ComicIssueBottomSection comicIssue={comicIssue} />
-          </ComicIssueHeader>
-          {comicIssue.isSecondarySaleActive && <p>secondary market</p>}
-        </div>
-      </main>
-    </>
+    <BaseLayout>
+      <ComicIssueBanner cover={comicIssue.cover} />
+      <ComicIssueHeader>
+        <CandyMachineStoreProvider comicIssue={comicIssue}>
+          <ComicIssueInfoSection comicIssue={comicIssue} />
+        </CandyMachineStoreProvider>
+        <ComicIssueBottomSection comicIssue={comicIssue} />
+      </ComicIssueHeader>
+      {comicIssue.isSecondarySaleActive && <p>secondary market</p>}
+    </BaseLayout>
   )
 }
