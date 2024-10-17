6792f1e3f5d04ee3ddc256886c9c12712f41628e
Frank Wang <frank7300228@gmail.com>
2024-10-17 19:21:24 +0800

    Integration by Circle's dev (#23)

--- src/app/(unauthenticated)/circle/page.tsx
+++ src/app/(unauthenticated)/circle/page.tsx
@@ -2,67 +2,30 @@
 
 import { createUserForSocialLogin } from '@/app/lib/actions/circle'
 import { Button } from '@/components/ui'
-import { W3SSdk } from '@circle-fin/w3s-pw-web-sdk'
-import {
-  SocialLoginProvider,
-  SocialLoginResult,
-  Error as CircleError,
-  EmailLoginResult,
-} from '@circle-fin/w3s-pw-web-sdk/dist/src/types'
-import { useEffect } from 'react'
-
-const appId = process.env.NEXT_PUBLIC_CIRCLE_APP_ID ?? ''
-
-let sdk: W3SSdk
+import { useSdk } from '@/contexts/Sdk'
+import { SocialLoginProvider } from '@circle-fin/w3s-pw-web-sdk/dist/src/types'
+import { useCallback } from 'react'
 
 export default function CirclePage() {
-  const onSuccessLogin = (error: CircleError | undefined, result: SocialLoginResult | EmailLoginResult | undefined) => {
-    console.log(`inside on success login`)
-    if (error) {
-      console.error(error)
+  const { sdk } = useSdk()
+
+  const onLogin = useCallback(async () => {
+    if (!sdk) {
       return
     }
-    console.log(result)
-    localStorage.setItem('userToken', result?.userToken ?? '')
-    localStorage.setItem('encryptionKey', result?.encryptionKey ?? '')
-  }
 
-  useEffect(() => {
-    sdk = new W3SSdk(
-      {
-        appSettings: {
-          appId,
-        },
-      },
-      onSuccessLogin
-    )
-  }, [])
-
-  const onLogin = async () => {
     const deviceId = await sdk.getDeviceId()
-    localStorage.setItem('deviceId', deviceId)
     const response = await createUserForSocialLogin(deviceId)
 
     if (!response) {
       return
     }
 
-    sdk.updateConfigs(
-      {
-        appSettings: { appId },
-        loginConfigs: {
-          deviceToken: response.deviceToken,
-          deviceEncryptionKey: response.deviceEncryptionKey ?? '',
-          google: {
-            clientId: `${process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID}`,
-            redirectUri: `${process.env.NEXT_PUBLIC_SITE_URL}/circle`,
-          },
-        },
-      },
-      onSuccessLogin
-    )
-    sdk.performLogin(SocialLoginProvider.GOOGLE)
-  }
+    localStorage.setItem('deviceToken', response.deviceToken)
+    localStorage.setItem('deviceEncryptionKey', response.deviceEncryptionKey ?? '')
+
+    sdk.performLogin(SocialLoginProvider.FACEBOOK)
+  }, [sdk])
 
   return (
     <div className='flex flex-col gap-6'>
