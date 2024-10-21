659598ead4f2a0a3d046b86d960570c001a1484e
d-reader-luka <luka.crnogorac@dreader.io>
2024-10-21 10:50:58 +0200

    use cn merge util

--- src/components/shared/ProjectHeader.tsx
+++ src/components/shared/ProjectHeader.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils'
 import React from 'react'
 
 type ProjectHeaderProps = {
@@ -9,10 +10,10 @@ type ProjectHeaderProps = {
 export const ProjectHeader: React.FC<ProjectHeaderProps> = ({ title, subtitle, className }) => {
   return (
     <div
-      className={
-        'flex flex-col justify-start items-start gap-4 py-6 pl-2 md:py-0 md:pt-2 md:justify-center md:items-center md:pl-0 md:pb-[34px] ' +
+      className={cn(
+        'flex flex-col justify-start items-start gap-4 py-6 pl-2 md:py-0 md:pt-2 md:justify-center md:items-center md:pl-0 md:pb-[34px]',
         className
-      }
+      )}
     >
       <h1 className='text-white text-2xl font-semibold leading-normal tracking-tight md:tracking-tight md:leading-none md:text-[40px]'>
         {title}
