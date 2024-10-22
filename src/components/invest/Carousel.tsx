698026d450073353830219c147347c4b629be752
d-reader-luka <luka.crnogorac@dreader.io>
2024-10-22 16:22:06 +0200

    fix: horizontal scroll on invest page

--- src/components/invest/Carousel.tsx
+++ src/components/invest/Carousel.tsx
@@ -83,7 +83,7 @@ const DetailsSection: React.FC<{ slide: InvestSlide }> = ({ slide }) => (
       src={slide.logo}
       width={290}
       height={130}
-      className='self-center sm:self-start w-40 md:w-[290px]'
+      className='self-center sm:self-start max-h-32 md:max-w-[290px]l w-auto'
     />
     <div className='flex flex-col justify-end gap-4 md:gap-6 max-w-[550px]'>
       <h1 className='text-xl md:text-[40px] leading-[20px] md:leading-[40px] font-semibold tracking-[0.08px] md:mb-4 font-obviouslyNarrow'>
