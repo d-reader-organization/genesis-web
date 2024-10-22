2019c9484c9ea408276499cbbbf5ea311eb46214
Karlo Hubak <103487011+khubak@users.noreply.github.com>
2024-10-22 09:32:37 +0200

    fix: payout card mobile design
    * projectpayoutcard update
    
    * fix: payoutCard update
    
    * fix: payout card update
    
    * Update ProjectPayoutCard.tsx
    
    * fix: project payout card mobile

--- src/components/payout/ProjectDescription.tsx
+++
@@ -1,9 +0,0 @@
-import { ProjectPayout } from "@/models/project"
-
-type Props = {
-  summary: ProjectPayout['summary']
-}
-
-export const ProjectDescription: React.FC<Props> = ({ summary }) => {
-  return <div className='w-full flex flex-col justify-start items-start'>{summary}</div>
-}
---
+++ src/components/payout/ProjectSummary.tsx
@@ -0,0 +1,9 @@
+import { ProjectPayout } from "@/models/project"
+
+type Props = {
+  summary: ProjectPayout['summary']
+}
+
+export const ProjectSummary: React.FC<Props> = ({ summary }) => {
+  return <div className='w-full flex flex-col justify-start items-start max-md:text-sm'>{summary}</div>
+}
