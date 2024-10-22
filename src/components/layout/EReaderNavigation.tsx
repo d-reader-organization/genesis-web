fa57a4af9a2394865bc289b1f89a9c5286e91b3a
d-reader-luka <luka.crnogorac@dreader.io>
2024-10-22 16:09:44 +0200

    chore: back button on ereader, feedback improvements

--- src/components/layout/EReaderNavigation.tsx
+++ src/components/layout/EReaderNavigation.tsx
@@ -7,6 +7,9 @@ import ArrowRightIcon from 'public/assets/vector-icons/arrow-right.svg'
 import Link from 'next/link'
 import { RoutePath } from '@/enums/routePath'
 import { InfoListActions } from '../shared/InfoListActions'
+import { useRouter } from 'next/navigation'
+import { Button } from '../ui'
+import { cn } from '@/lib/utils'
 
 type Props = {
   comicIssue: ComicIssue
@@ -16,7 +19,7 @@ type Props = {
 export const EReaderNavigation: React.FC<Props> = ({ comicIssue, hideNavigation = false }) => {
   const navigationLinkStyle = 'hover:text-yellow-500 hover:[&>*]:text-yellow-500 text-lg'
   const arrowRightReversedStyle = 'h-[18px] text-text-color rotate-180'
-
+  const { back } = useRouter()
   const commands = (
     <div className='justify-end '>
       <InfoListActions
@@ -32,43 +35,26 @@ export const EReaderNavigation: React.FC<Props> = ({ comicIssue, hideNavigation 
   )
 
   return (
-    <>
-      <div
-        className={clsx(
-          'bg-grey-600 border-none sticky transition-[colors_300,opacity_500] p-0',
-          hideNavigation && 'opacity-0'
-        )}
-      >
-        <div className='w-full mx-auto h-[72px] max-w-screen-xl flex items-center'>
-          <div className='flex items-center justify-between max-w-screen-md mx-auto w-full'>
-            <Link href={RoutePath.ComicIssue(comicIssue.id)} className={navigationLinkStyle}>
-              <ArrowRightIcon className={arrowRightReversedStyle} />
-            </Link>
-            <Link href={RoutePath.ComicIssue(comicIssue.id)} className={clsx('flex gap-1', navigationLinkStyle)}>
-              <div className='hidden sm:block'>
-                <strong>EP {comicIssue.number}</strong>&nbsp;&nbsp;
-              </div>
-              <span className='text-grey-100'>{comicIssue.title}</span>
-            </Link>
-            {commands}
-          </div>
+    <div
+      className={clsx(
+        'bg-grey-600 border-none sticky transition-[colors_300,opacity_500] p-0',
+        hideNavigation && 'opacity-0'
+      )}
+    >
+      <div className='w-full mx-auto h-[72px] max-w-screen-xl flex items-center'>
+        <div className='flex items-center justify-between max-w-screen-md mx-auto w-full'>
+          <Button variant='ghost' onClick={() => back()} className={cn('w-fit', navigationLinkStyle)}>
+            <ArrowRightIcon className={arrowRightReversedStyle} />
+          </Button>
+          <Link href={RoutePath.ComicIssue(comicIssue.id)} className={clsx('flex gap-1', navigationLinkStyle)}>
+            <div className='hidden sm:block'>
+              <strong>EP {comicIssue.number}</strong>&nbsp;&nbsp;
+            </div>
+            <span className='text-grey-100'>{comicIssue.title}</span>
+          </Link>
+          {commands}
         </div>
       </div>
-
-      {/* <div className='sm:hidden'>
-        <div
-          className={clsx(
-            'bg-grey-600 border-none sticky transition-[colors_300,opacity_500] p-0',
-            hideNavigation && 'opacity-0'
-          )}
-        >
-          <div className='w-full mx-auto h-[72px] max-w-screen-xl'>
-            <div className='flex items-center justify-between max-w-screen-md mx-auto w-full'>{commands}</div>
-          </div>
-        </div>
-      </div> */}
-
-      {/* <div ref={pageBottomRef} className='absolute bottom-0 size-[72px]' /> */}
-    </>
+    </div>
   )
 }
