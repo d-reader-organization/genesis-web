9cbd991633077f81b9956203402744d95bf79f17
Karlo Hubak <103487011+khubak@users.noreply.github.com>
2024-10-29 08:57:55 +0100

    discord icon fix

--- src/components/shared/ProductSocials.tsx
+++ src/components/shared/ProductSocials.tsx
@@ -2,19 +2,29 @@ import React from 'react'
 import TwitterIcon from 'public/assets/vector-icons/twitter-icon.svg'
 import InstagramIcon from 'public/assets/vector-icons/instagram-icon.svg'
 import DiscordIcon from 'public/assets/vector-icons/discord-icon.svg'
-import Link from 'next/link'
+import { IconLink } from './IconLink'
 import { DISCORD_LINK, INSTAGRAM_LINK, TWITTER_LINK } from '@/constants/links'
+import { DEFAULT_ICON_CLASSNAME } from '@/constants/defaultClassName'
 
 export const ProductSocials: React.FC = () => (
-  <div className='flex justify-between items-center'>
-    <Link href={TWITTER_LINK}>
-      <TwitterIcon className='size-4' />
-    </Link>
-    <Link href={INSTAGRAM_LINK}>
-      <InstagramIcon className='size-4' />
-    </Link>
-    <Link href={DISCORD_LINK}>
-      <DiscordIcon />
-    </Link>
+  <div className='flex justify-between items-center text-grey-100'>
+    <IconLink
+      href={TWITTER_LINK}
+      Icon={TwitterIcon}
+      iconClassName={DEFAULT_ICON_CLASSNAME}
+      blank
+    />
+    <IconLink
+      href={INSTAGRAM_LINK}
+      Icon={InstagramIcon}
+      iconClassName={DEFAULT_ICON_CLASSNAME}
+      blank
+    />
+    <IconLink
+      href={DISCORD_LINK}
+      Icon={DiscordIcon}
+      iconClassName={DEFAULT_ICON_CLASSNAME}
+      blank
+    />
   </div>
 )
