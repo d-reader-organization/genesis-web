65d2c164517f6997f81c5661a283cbd0799c2f75
Karlo Hubak <103487011+khubak@users.noreply.github.com>
2024-10-29 08:57:55 +0100

    discord icon fix

--- src/components/shared/ProductSocials.tsx
+++ src/components/shared/ProductSocials.tsx
@@ -2,19 +2,13 @@ import React from 'react'
 import TwitterIcon from 'public/assets/vector-icons/twitter-icon.svg'
 import InstagramIcon from 'public/assets/vector-icons/instagram-icon.svg'
 import DiscordIcon from 'public/assets/vector-icons/discord-icon.svg'
-import Link from 'next/link'
 import { DISCORD_LINK, INSTAGRAM_LINK, TWITTER_LINK } from '@/constants/general'
+import { IconLink } from './IconLink'
 
 export const ProductSocials: React.FC = () => (
-  <div className='flex justify-between items-center'>
-    <Link href={TWITTER_LINK}>
-      <TwitterIcon className='size-4' />
-    </Link>
-    <Link href={INSTAGRAM_LINK}>
-      <InstagramIcon className='size-4' />
-    </Link>
-    <Link href={DISCORD_LINK}>
-      <DiscordIcon />
-    </Link>
+  <div className='flex justify-between items-center text-grey-200'>
+    <IconLink href={TWITTER_LINK} Icon={TwitterIcon} blank />
+    <IconLink href={INSTAGRAM_LINK} Icon={InstagramIcon} blank />
+    <IconLink href={DISCORD_LINK} Icon={DiscordIcon} blank />
   </div>
 )
