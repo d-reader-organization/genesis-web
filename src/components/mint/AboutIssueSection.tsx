139a6599c788220a75e475a41f465eb9d2218c75
athar <athar.mohammad@dreader.io>
2024-10-30 19:14:54 +0530

    chore: add external link to tensor market for comic issues

--- src/components/mint/AboutIssueSection.tsx
+++ src/components/mint/AboutIssueSection.tsx
@@ -20,21 +20,33 @@ export const AboutIssueSection: React.FC<Props> = ({ comicIssue }) => (
     <GenreTags genres={comicIssue.genres ?? []} />
     <ExpandableText className='max-w-[440px] lg:max-w-[486px]' text={comicIssue.description} />
     <div className='flex justify-between'>
-      <div className='flex gap-4 items-center'>
+      <div className='flex gap-4 items-end'>
         <div className='text-base font-medium leading-[22.4px]'>
           <span>{comicIssue.stats?.totalPagesCount}&nbsp;</span>
           <span className='text-grey-100'>pages</span>
         </div>
         <AudienceWidget audience={comicIssue.comic?.audienceType ?? AudienceType.Everyone} />
       </div>
-      <Link
-        className='max-h-9 px-3 py-2 flex gap-2 justify-center items-center rounded-lg bg-grey-500'
-        href={RoutePath.Comic(comicIssue.comicSlug)}
-        target='_blank'
-      >
-        <ExternalLink className='text-grey-100' size={20} />
-        <span className='text-base font-medium leading-[22.4px] text-grey-100'>Explore series</span>
-      </Link>
+      <div className='flex flex-col gap-2'>
+        <Link
+          className='max-h-9 px-3 py-2 flex gap-2 justify-center items-center rounded-lg bg-grey-500'
+          href={RoutePath.Comic(comicIssue.comicSlug)}
+          target='_blank'
+        >
+          <ExternalLink className='text-grey-100' size={20} />
+          <span className='text-base font-medium leading-[22.4px] text-grey-100'>Explore series</span>
+        </Link>
+        {comicIssue.collectionAddress && !comicIssue.activeCandyMachineAddress && (
+          <Link
+            className='max-h-9 px-3 py-2 flex gap-2 justify-center items-center rounded-lg bg-yellow-500'
+            href={`https://www.tensor.trade/trade/${comicIssue.collectionAddress}`}
+            target='_blank'
+          >
+            <ExternalLink className='text-black' size={20} />
+            <span className='text-base font-medium leading-[22.4px text-black'>Buy From Tensor</span>
+          </Link>
+        )}
+      </div>
     </div>
     <Divider />
     <CreatorInfoLink creator={comicIssue.creator} />
