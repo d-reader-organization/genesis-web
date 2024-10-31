2a85fd5389aeb20b9c91241c2cc732e679f55139
Athar Mohammad <athar.mohammad@dreader.io>
2024-10-31 15:03:18 +0530

    chore: redirect after registration (#55)
    * chore: redirect after registration
    
    * fix: fomatting
    
    * fix: use suspense wrapper for register link
    
    * chore: remove white border from register tabs
    
    ---------
    
    Co-authored-by: d-reader-luka <luka.crnogorac@dreader.io>

---
+++ src/components/register/RegisterLink.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Link from 'next/link'
+import { RoutePath } from '@/enums/routePath'
+import { Text } from '@/components/ui'
+import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+
+export const RegisterLink: React.FC = () => {
+  const searchParams = useSearchParams()
+  return (
+    <Link className='flex hover:brightness-150' href={`${RoutePath.Register}?${searchParams}`}>
+      <Text as='p' styleVariant='body-normal' className='text-grey-100'>
+        Don&apos;t have an account?&nbsp;
+      </Text>
+      <Text as='p' styleVariant='body-normal' className='text-important-color'>
+        Register here
+      </Text>
+    </Link>
+  )
+}
+
+export const RegisterLinkWrapper: React.FC = () => (
+  <Suspense>
+    <RegisterLink />
+  </Suspense>
+)
