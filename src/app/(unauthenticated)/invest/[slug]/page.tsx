6d049bfe637e3b2c3c1fef79acc24699a647f4fa
d-reader-josip <josip.volarevic@dreader.io>
2024-11-08 18:53:19 -0600

    chore: update metadata for invest slug

--- src/app/(unauthenticated)/invest/[slug]/page.tsx
+++ src/app/(unauthenticated)/invest/[slug]/page.tsx
@@ -8,9 +8,41 @@ import { notFound } from 'next/navigation'
 import { fetchProject } from '@/app/lib/api/invest/queries'
 import { ProjectInvestDialog } from '@/components/shared/dialogs/ProjectInvestDialog'
 import { InterestUpdatesCard } from '@/components/invest/InterestUpdatesCard'
+import { Metadata } from 'next'
 
 type Props = {
   params: { slug: string }
+}
+
+export const metadata: Metadata = {
+  title: 'Genesis',
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL as string),
+  description:
+    'Vote on new talent. back breakthrough stories! Support creators which you feel have the potential to produce, direct & sell a captivating story.',
+  keywords: 'NFT, asset, dReader, dPublisher, Comic, Solana, SOL, mint, collection, manga, manwha',
+  openGraph: {
+    type: 'website',
+    title: 'Genesis',
+    description:
+      'Vote on new talent. back breakthrough stories! Support creators which you feel have the potential to produce, direct & sell a captivating story.',
+    images: '/assets/images/metadata-invest.png',
+    url: process.env.NEXT_PUBLIC_SITE_URL + '/invest',
+    siteName: 'Genesis',
+  },
+  appleWebApp: {
+    title: 'Genesis',
+    startupImage: '/assets/apple-touch-icon.png',
+  },
+  twitter: {
+    title: 'Genesis',
+    description:
+      'Vote on new talent. back breakthrough stories! Support creators which you feel have the potential to produce, direct & sell a captivating story.',
+    card: 'summary_large_image',
+    site: '@GenesisApp',
+    creator: '@GenesisApp',
+    images: '/assets/images/metadata-invest.png',
+  },
+  manifest: '/manifest.json',
 }
 
 export default async function ProjectInvestPage({ params }: Props) {
