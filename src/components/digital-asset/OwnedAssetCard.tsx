8aa2c16d7e1911187a31b6cca26894ec32b1af77
d-reader-luka <125265091+d-reader-luka@users.noreply.github.com>
2024-11-28 09:16:33 +0100

    chore: update cards (#67)
    * chore: card changes
    
    * add episodes count on comic card
    
    * delete comment

--- src/components/digital-asset/OwnedAssetCard.tsx
+++ src/components/digital-asset/OwnedAssetCard.tsx
@@ -3,24 +3,27 @@ import { Asset } from '@/models/asset'
 import Image from 'next/image'
 import React from 'react'
 import { StateChip } from '../shared/chips/State'
+import { CardBorderWrapper } from '../shared/CardBorderWrapper'
 
 type Props = {
   asset: Asset
 }
 
 export const OwnedAssetCard: React.FC<Props> = ({ asset }) => (
-  <div className='flex flex-col items-center hover:scale-105'>
-    <Image
-      alt={`owned ${asset.name} cover`}
-      width={223}
-      height={322}
-      src={asset.image}
-      className='w-auto max-h-[322px] h-full'
-    />
-    <div className='flex items-center -mt-3.5'>
-      <RarityChip rarity={asset.rarity} />
-      {asset.isUsed ? <StateChip state='used' /> : <StateChip state='mint' />}
-      {asset.isSigned ? <StateChip state='signed' /> : null}
+  <CardBorderWrapper className='rounded-xl'>
+    <div className='flex flex-col items-center hover:brightness-105'>
+      <Image
+        alt={`owned ${asset.name} cover`}
+        width={223}
+        height={322}
+        src={asset.image}
+        className='w-auto max-h-[322px] h-full'
+      />
+      <div className='flex items-center -mt-3.5'>
+        <RarityChip rarity={asset.rarity} />
+        {asset.isUsed ? <StateChip state='used' /> : <StateChip state='mint' />}
+        {asset.isSigned ? <StateChip state='signed' /> : null}
+      </div>
     </div>
-  </div>
+  </CardBorderWrapper>
 )
