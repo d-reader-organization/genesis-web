7e2132e6452bc8d23ec4af714e8fb5b496fdc431
d-reader-luka <luka.crnogorac@dreader.io>
2024-12-10 15:55:52 +0100

    chore: update is web view check

--- src/components/shared/buttons/GoogleSignInButton.tsx
+++ src/components/shared/buttons/GoogleSignInButton.tsx
@@ -11,16 +11,18 @@ type Props = {
 } & Pick<React.HTMLAttributes<HTMLButtonElement>, 'className'>
 
 export const GoogleSignInButton: React.FC<Props> = ({ buttonText = 'Sign in with google', className }) => {
+  const isWebView = () => {
+    const ua = navigator.userAgent.toLowerCase()
+    return (
+      ua.includes('wv') ||
+      ua.includes('webview') ||
+      ((ua.includes('iphone') || ua.includes('ipad')) && !ua.includes('safari')) ||
+      (ua.includes('android') && ua.includes('version/')) ||
+      typeof window.phantom !== 'undefined'
+    )
+  }
   const handleSignIn = (providerId: string) => {
-    const userAgent = navigator.userAgent.toLowerCase()
-    const isWebView =
-      userAgent.includes('wv') ||
-      userAgent.includes('webview') ||
-      typeof window.phantom !== undefined ||
-      userAgent.includes('snap') ||
-      userAgent.includes('snapchat')
-
-    if (isWebView) {
+    if (isWebView()) {
       const authUrl = `/api/auth/signin/${providerId}?callbackUrl=${encodeURIComponent(window.location.origin)}`
       window.open(authUrl, '_blank')
     } else {
