6f9bf15348267bcce7f43853edbde5128f034005
d-reader-luka <luka.crnogorac@dreader.io>
2024-12-10 12:36:54 +0100

    chore: try sign in with google in webview

--- src/components/shared/buttons/GoogleSignInButton.tsx
+++ src/components/shared/buttons/GoogleSignInButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from '../../ui/Button'
 import GoogleLogoIcon from 'public/assets/vector-icons/google-logo.svg'
 import { signIn } from 'next-auth/react'
@@ -10,11 +10,28 @@ type Props = {
   buttonText?: string
 }
 
-export const GoogleSignInButton: React.FC<Props> = ({ buttonText = 'Sign in with google' }) => (
-  <Button onClick={() => signIn('google')} type='button' className='text-grey-100' variant='outline'>
-    <GoogleLogoIcon className='mr-1 h-[19px]' />
-    <Text as='p' fontWeight='medium' styleVariant='body-normal'>
-      {buttonText}
-    </Text>
-  </Button>
-)
+export const GoogleSignInButton: React.FC<Props> = ({ buttonText = 'Sign in with google' }) => {
+  const [isWebView, setIsWebView] = useState(false)
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase()
+    setIsWebView(userAgent.includes('wv') || userAgent.includes('webview'))
+  }, [])
+
+  const handleSignIn = (providerId: string) => {
+    if (isWebView) {
+      const authUrl = `/api/auth/signin/${providerId}?callbackUrl=${encodeURIComponent(window.location.origin)}`
+      window.open(authUrl, '_blank')
+    } else {
+      signIn(providerId)
+    }
+  }
+  return (
+    <Button onClick={() => handleSignIn('google')} type='button' className='text-grey-100' variant='outline'>
+      <GoogleLogoIcon className='mr-1 h-[19px]' />
+      <Text as='p' fontWeight='medium' styleVariant='body-normal'>
+        {buttonText}
+      </Text>
+    </Button>
+  )
+}
