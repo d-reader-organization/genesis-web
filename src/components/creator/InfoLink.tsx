f5bc2dbccae9d97eda154c602ca30f4fd8a92106
d-reader-luka <luka.crnogorac@dreader.io>
2024-12-13 14:29:46 +0100

    chore: add href on creator info link component

--- src/components/creator/InfoLink.tsx
+++ src/components/creator/InfoLink.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Creator } from '@/models/creator'
 import { AvatarImage } from '../shared/AvatarImage'
 import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { RoutePath } from '@/enums/routePath'
 
 type Props = React.HTMLAttributes<HTMLDivElement> & {
   creator: Pick<Creator, 'name' | 'slug' | 'isVerified' | 'avatar'> | undefined
@@ -9,11 +11,11 @@ type Props = React.HTMLAttributes<HTMLDivElement> & {
 
 export const CreatorInfoLink: React.FC<Props> = ({ className, creator }) =>
   !!creator ? (
-    <div className={cn('flex items-center gap-3 max-h-[42px]', className)}>
+    <Link href={RoutePath.Creator(creator.slug)} className={cn('flex items-center gap-3 max-h-[42px]', className)}>
       <AvatarImage size='medium' src={creator.avatar} />
       <div className='flex flex-col gap-1 justify-center items-start'>
         <span className='text-xs font-medium leading-normal text-grey-200'>Author</span>
         <span className='text-base font-bold leading-[22.4px]'>{creator.name}</span>
       </div>
-    </div>
+    </Link>
   ) : null
